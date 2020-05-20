764503140effe947af91e07730d5dec538ef081c
Junászka Klaudia <klausz8801@gmail.com>
2020-05-20 13:17:28 +0200

    indexjs to team1

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // teszt komment
+
+// create main function
+const map = require('./map');
+const moving = require('./moving');
+
+const main = () => {
+console.log('Hello gamer!');
+    const arr = map.generateMap(5, 5);
+console.log('You are positioned at ', );
+console.log('To moving type one of the nexts: x, y');
+setInterval(() => {
+	map.fillMap();
+	placeCharacter();
+        movment.moving();
+};
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.resume();
+    stdin.setEncoding('utf8');
+    stdin.on('data', (key) => {
+        if (key === 'q') {
+            process.exit();
+        };
+main();
