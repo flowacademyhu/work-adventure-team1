e58cdc74aabba8471c4f48831ba172c19a75f9b2
javierfurus <javierfurus@gmail.com>
2020-05-20 12:00:10 +0200

    First version and added comments

--- functions/movement.js
+++ functions/movement.js
@@ -1,3 +1,4 @@
+let character = {}; // Placeholder; this should equal to the value set by placeCharacter
 const moving = (arr, direction) => {
   const coordinates = () => {
     console.log(`Az X:${character.x}, Y:${character.y} ${character.name}`);
@@ -7,10 +8,10 @@ const moving = (arr, direction) => {
   if (direction === 'w') {
     for (let i = 0; i < height - 1; i++) { // We check if the character has reached the bottom or not. i++ because we are moving upwards.
       for (let j = 0; j < arr[i].length; j++) { // Here we check if we have hit the width limits.;
-        if (arr[i + 1][j] === character) { // We delete the characzer in the previous line
-          character = arr[i][j];
-          arr[i][j] = character; // We add a character to where we are standing
-          coordinates();// Let's check where we are standing.
+        if (arr[i + 1][j] === character) { // We check where we are with the character
+          character = arr[i][j]; // We make it pick up the value of the cell
+          arr[i][j] = character; // We put the character to where we are standing
+          coordinates();
           return;
         }
       }
@@ -19,7 +20,7 @@ const moving = (arr, direction) => {
   if (direction === 's') {
     for (let i = height - 1; i > 0; i--) {
       for (let j = 0; j < arr[i].length; j++) {
-        if (arr[i - 1][j] === character) { // The character goes one step right (-1y) so that means that the one before it (+1y) gets deleted
+        if (arr[i - 1][j] === character) {
           character = arr[i][j];
           arr[i][j] = character;
           coordinates();
@@ -31,7 +32,7 @@ const moving = (arr, direction) => {
   if (direction === 'd') {
     for (let i = 0; i < height; i++) {
       for (let j = width - 1; j > 0; j--) {
-        if (arr[i][j - 1] === character) { // The character goes one step right (-1y) so that means that the one before it (+1y) gets deleted
+        if (arr[i][j - 1] === character) {
           character = arr[i][j];
           arr[i][j] = character;
           coordinates();
@@ -43,7 +44,7 @@ const moving = (arr, direction) => {
   if (direction === 'a') {
     for (let i = 0; i < height; i++) {
       for (let j = 0; j < width - 1; j++) {
-        if (arr[i][j + 1] === character) { // The character goes one step right (-1y) so that means that the one before it (+1y) gets deleted
+        if (arr[i][j + 1] === character) {
           character = arr[i][j];
           arr[i][j] = character;
           coordinates();
