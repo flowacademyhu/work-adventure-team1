bc3c8e795f9705fae887a4999de18a9de09093a0
Junászka Klaudia <klausz8801@gmail.com>
2020-05-20 17:35:12 +0200

    indexjs magyar

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const map = require('./functions/map');
 const moving = require('./functions/movement');
 
 const main = () => {
-  console.log('Hello gamer!');
+  console.log('Hello játékos!');
   const mapArr = map.generateMap(5, 5);
-  console.log('To moving type one of the nexts: w, a, s, d');
+  console.log('Mozgáshoz a következő betűkből válassz ki egyet: w, a, s, d');
 
   map.fillMap(mapArr);
   const characterLocation = map.placeCharacter(mapArr);
@@ -21,19 +21,19 @@ const main = () => {
     if (key === 'w') {
       console.clear();
       moving.mov(mapArr, key, characterLocation);
-      console.log('You turned North');
+      console.log('Északra fordultál');
     } else if (key === 'a') {
       console.clear();
       moving.mov(mapArr, key, characterLocation);
-      console.log('You turned West');
+      console.log('Nyugatra fordultál');
     } else if (key === 's') {
       console.clear();
       moving.mov(mapArr, key, characterLocation);
-      console.log('You turned South');
+      console.log('Délre fordultál');
     } else if (key === 'd') {
       console.clear();
       moving.mov(mapArr, key, characterLocation);
-      console.log('You turned East');
+      console.log('Keletre fordultál');
     }
   });
 };
