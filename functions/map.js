04129fcb0e27029b1a5f527b059028588f5e0932
Juhasz Peter <b.juhaszpeter@gmail.com>
2020-05-20 13:19:14 +0200

    add placeCaracter function

---
+++ functions/map.js
@@ -0,0 +1,37 @@
+
+const generateMap = (width, height) => {
+  const arr = new Array(height);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(width);
+  }
+  return arr;
+};
+
+const fillMap = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      const coordinate = { x: (i + 1), y: (j + 1), name: 'Koordinátán állsz!' };
+      arr[i][j] = coordinate;
+    }
+  }
+  const rbl = arr[Math.floor(Math.random() * arr.length)][Math.floor(Math.random() * arr[0].length)];
+  const basicLocation = rbl.coordinate;
+
+  return basicLocation;
+};
+
+const placeCaracter = (arr) => {
+  const rbl = arr[Math.floor(Math.random() * arr.length)][Math.floor(Math.random() * arr[0].length)];
+  const basicLocation = { rbl };
+  return basicLocation;
+};
+
+const test = generateMap(5, 5);
+fillMap(test);
+console.log(placeCaracter(test));
+
+module.exports = {
+  generateMap,
+  fillMap
+
+};
