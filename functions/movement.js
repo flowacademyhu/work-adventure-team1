83f8abe870547fac4a2338d150fae34d9de1d403
javierfurus <javierfurus@gmail.com>
2020-05-20 15:25:23 +0200

    refined the function and added bugfixes

--- functions/movement.js
+++ functions/movement.js
@@ -1,26 +1,33 @@
-let character = {}; // Placeholder; this should equal to the value set by placeCharacter
-const moving = (arr, direction) => {
+let isInitialised = false;
+let character = {};
+const moving = (arr, direction, initialLocation) => {
+  console.log(isInitialised);
+  if (!isInitialised) {
+    character = arr[initialLocation.x][initialLocation.y];
+    isInitialised = true;
+    return;
+  }
   const coordinates = () => {
     console.log(`Az X:${character.x}, Y:${character.y} ${character.name}`);
   };
   const height = arr.length;
   const width = arr[0].length;
   if (direction === 'w') {
-    for (let i = 0; i < height - 1; i++) { // We check if the character has reached the bottom or not. i++ because we are moving upwards.
+    for (let i = 0; i < height - 1; i++) { // We check if the character has reached the top or not. i++ because we are moving upwards.
       for (let j = 0; j < arr[i].length; j++) { // Here we check if we have hit the width limits.;
-        if (arr[i + 1][j] === character) { // We check where we are with the character
-          character = arr[i][j]; // We make it pick up the value of the cell
-          arr[i][j] = character; // We put the character to where we are standing
-          coordinates();
+        if (arr[i + 1][j] === character) { // We move up so we check the cell below us
+          character = arr[i][j];
+          arr[i][j] = character; // We add a character to the desired location
+          coordinates();// Let's check where we are standing.
           return;
         }
       }
     }
   }
   if (direction === 's') {
-    for (let i = height - 1; i > 0; i--) {
+    for (let i = height - 1; i > 0; i--) { // We check if the character has reached the bottom or not. i-- because we are moving downwards.
       for (let j = 0; j < arr[i].length; j++) {
-        if (arr[i - 1][j] === character) {
+        if (arr[i - 1][j] === character) { // Here we check what is above us
           character = arr[i][j];
           arr[i][j] = character;
           coordinates();
@@ -31,8 +38,8 @@ const moving = (arr, direction) => {
   }
   if (direction === 'd') {
     for (let i = 0; i < height; i++) {
-      for (let j = width - 1; j > 0; j--) {
-        if (arr[i][j - 1] === character) {
+      for (let j = width - 1; j > 0; j--) { // We check if the character has reached the sides or not. j-- because we are moving to the right
+        if (arr[i][j - 1] === character) { // What is left of us
           character = arr[i][j];
           arr[i][j] = character;
           coordinates();
@@ -43,8 +50,8 @@ const moving = (arr, direction) => {
   }
   if (direction === 'a') {
     for (let i = 0; i < height; i++) {
-      for (let j = 0; j < width - 1; j++) {
-        if (arr[i][j + 1] === character) {
+      for (let j = 0; j < width - 1; j++) { // We check if the character has reached the sides or not. j++ because we are moving to the left
+        if (arr[i][j + 1] === character) { // And what is right of us
           character = arr[i][j];
           arr[i][j] = character;
           coordinates();
