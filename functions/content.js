d8c94454b087604d97d49fabc55b192a45d17621
Junászka Klaudia <klausz8801@gmail.com>
2020-05-21 12:52:41 +0200

    create contentjs

--- functions/content.js
+++ functions/content.js
@@ -1,1 +1,109 @@
 // content comment
+const contentArr = [
+  [
+    {
+      description: '',
+      life: true
+    },
+    {
+      description: '',
+      life: true
+    },
+    {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: true
+    },
+  {
+      description: '',
+      life: false
+    },
+  {
+      description: '',
+      life: false
+    },
+  {
+      description: '',
+      life: false
+    },
+  {
+      description: '',
+      life: false
+    },
+  {
+      description: '',
+      life: false
+    },
+  ]
+];
