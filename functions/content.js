a628b18320f1f048d2611f37b6fdeaa4956f3c2b
Juhasz Peter <b.juhaszpeter@gmail.com>
2020-05-21 13:25:36 +0200

    fill content in discriptions

--- functions/content.js
+++ functions/content.js
@@ -1,107 +1,108 @@
 // content comment
 const contentArr = [
-      {
-      description: '',
-      life: true
-    },
-    {
-      description: '',
-      life: true
-    },
-    {
-      description: '',
-      life: true
-    },
   {
-      description: '',
-      life: true
-    },
+    description: 'Követed a derengést és reményed valósággá válik. A sarkon túl egy földcsuszamlás utat nyit a hegyoldalba. Mélyet sóhajtasz a friss levegőben és elhagyod ezt a rettenetes hely. (Győztél! A játék véget ért! A kilépéshez nyomd meg a „q” bilentyűt!)',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'Kezded úgy érezni hogy nincs kiút… a bolyongásod végtelen… és a telefonod is egyre jobban merül… kiabálhatnál hátha meghallja valaki… inkább mégsem… mert hátha meghallja valaki...',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'A polcsokon lévő palackokat évszázados por fedi… talán bor lehet bennük… vagy valami más...',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'Nem tudod mióta lehetsz eszméletlen… és mi ez az íz a szádban… ez a szag az orrodban… olyan mint a… vér...',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'Térdre rogysz a fáradságtól… vajon mióta bolyongasz…? Órák… vagy napok óta…? Mi van ha hónapok…? és mivan ha örökre itt ragadsz...?',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'Végig fut a hideg a hátadon ahogy a víz a nyakadva cseppen… telefonod fényénél látod hogy a vízcseppek összegyűlnek a plafonon es egy bisonyos irányba tartanak...',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'A folyosó itt beomlott… más irányba kell keresned a kiutat tovább…',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'N’gai, ri gha’ghaa, bugg-shoggog…  N’gai, ri gha’ghaa, bugg-shoggog… magad sem tudod mióta mormolod a szavakat… sem azt hogy mit jelentenek...',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'A fémes csendőlés egy poros késtől származik ami a padlón hevert mielőtt bele nem rugtál… pengéje ívelt markolata pedig valamilyen kőből faragott… az övedre tűzöd hátha hasznát veszed a későbbiekben',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'a kőből készült asztalt valami sötét festés borítja… valamivel csúnyán bemocskolták sok sok évvel ezelőtt...',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'a torkod egésszen kiszáradt… a víz csobogása egyszerre tölt el reménnyel és kínnal…',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'Bele telik egy kis időbe mire meggyőzöd magad hogy az egerek nem valódiak… megdőrzsölöd a szemed és megtörlöd a homlokod… tovább kell menned...',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'öszeszorul a gyomrod mikor felismered a lábynomokat a porban… körbe - körbe jársz...',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'a víz csobogás erősödik… ez jó jel… ha követed talán kijutsz a vele együtt a felszínre...',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'Meg állsz hogy józanul végig gondold a helyzeted… kétségbe ejt hogy már képtelen vagy meghatározni honan jöttél és merre tartasz...',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'Megrettensz a recsenéstől de a lábad alatt összeropant csontoktól még inkább kiráz a hideg… vajon milyen lénynek lehetne ekkora csontjai…?',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'Sietned kell, a telefonod töltöttsége rohamosan fogy… és a levegő is mintha egyre fojtogatóbb lenne',
+    life: true
+  },
   {
-      description: '',
-      life: true
-    },
+    description: 'A bolyongás kezd egyre frusztrálóbb lenni. Mintha az árnyékod megmozdult volna… biztos csak a képzeleted játszik veled...',
+    life: true
+  },
   {
-      description: '',
-      life: false
-    },
+    description: 'A bolyongás kezd egyre frusztrálóbb lenni. Mintha az árnyékod megmozdult volna… biztos csak a képzeleted játszik veled...',
+    life: true
+  },
   {
-      description: '',
-      life: false
-    },
+    description: 'A falra festett motívumok egy ősi civilizáció letünt írásjelei lehetnek. De vajon mivel festették őket...?',
+    life: true
+  },
   {
-      description: '',
-      life: false
-    },
+    description: 'a hang a fejedben egyre erősödik … - N’gai, ri gha’ghaa, bugg-shoggog, y’hah: Yog-Sothoth, Yog-Sothoth... - és vailág forogni kezd...',
+    life: false
+  },
   {
-      description: '',
-      life: false
-    },
+    description: 'A falból kinövő zöld növény különleges illatot áraszt… el sem tudod képzelni hogyan képes életben maradni ide lent… émelyítő... ',
+    life: false
+  },
   {
-      description: '',
-      life: false
-    },
+    description: 'A falból kinövő zöld növény különleges illatot áraszt… el sem tudod képzelni hogyan képes életben maradni ide lent… émelyítő...',
+    life: false
+  },
+  {
+    description: 'Az emelvényen lévő könyvet ellepte a por… vajon mióta lehet itt…? Az írások benne kivehetelenek égis mintha belekúszna az elmédbe a mondanivalója… és amit mond az maga a borzalom...',
+    life: false
+  },
+  {
+    description: 'A kecske teteme bűzlik… alig bírod elviselni… az állat felemeli a fejét, rádnéz majd visszaejti a földre… egy pillanatra megijesz de aztán ismét beleharapsz a húsába...',
+    life: false
+  }
 ];
+
+module.exports = {
+  contentArr
+
+};
