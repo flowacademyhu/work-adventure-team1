66046f20c7dbc65c8bbff96ad6890a7058ed0152
javierfurus <javierfurus@gmail.com>
2020-05-21 17:27:58 +0200

    Reverting accidental deletion

--- index.js
+++ index.js
@@ -6,19 +6,12 @@ const context = require('./functions/context');
 const opLife = require('./functions/life');
 
 const main = () => {
-  console.log('--------------- RETTENET A FÖLD ALATT-------------------');
-  console.log(' ');
-
-  console.log('Nem is sjtetted hogy Nagyapád birtoka ilyen veszélyeket is rejt. \n Sajognak a csontjaid az esés miatt de szerencsére minden végtagod mozog. \n Ahogy az elméd kezd tisztulni rádöbbensz hogy olcsón megúsztad a zuhanást. \nKorom sötét van körülötted. Gyorsan a zsebedbe nyúlsz és előkapod a telefonodat. \nSzerencsére nem tört össze és tökéletesen működik. \nSajnos ilyen mélyen a föld alatt nincs térerő. De legalább világítani tudsz vele. Viszont a 22%os töltötség nem túl bizalomgerjesztő. \nAhogy körülnézel a telefonod fényében látod egy  régen az épület alá vájt kazamatarendszerbe kerültél ami egymást keresztező folyosók végeláthatatlan labirintusa. \nSajnos lehetetlen ott kimásznod ahonan lezuhantál. Az omlás túl magasan van. \nA légmozgás azonban arra utal hogy valahol kell, hogy legyen egy másik kijárata ennek a helynek. \nLassan azt is észreveszed hogy egy különös borzongató érzés kezd úrrálenni rajtad. Megrázod magad amitől megszűnik a hideg érzet de nem tudsz szabadulnia gondolattól \nhogy valami különös titokkal találtad szembe megad. \nNincs más hátra elindulsz a ősi labirintusban. \nMeg kell találnod a kiutat mielőtt lemerült a telefonod vagy mielőtt … de ebbe inkább nem is gondolsz bele');
-
+  console.log('Hello játékos!');
   const mapArr = map.generateMap(5, 5);
   const shuffledArr = map.shuffleArr(contentArr.contentArr);
   const mLife = 5;
-  console.log(' ');
 
   console.log('Mozgáshoz a következő betűkből válassz ki egyet: w, a, s, d');
-  console.log(' ');
-
   console.log('LIFE:', mLife);
 
   map.fillMap(mapArr, shuffledArr);
@@ -56,10 +49,6 @@ const main = () => {
       console.log(context.returnCon().content.description);
       console.log('LIFE:', mLife);
     }
-    console.log(' ');
-
-    console.log('Merre tovább?');
-    console.log('Mozgáshoz a következő betűkből válassz ki egyet: w, a, s, d');
     opLife.operationLife(context.returnCon().content.life, mLife);
     opLife.invLife(mLife);
   });
