aba5041cc07dd06d83e4c1fdcced2dffe3648281
javierfurus <javierfurus@gmail.com>
2020-05-21 17:42:59 +0200

    Finalising everything

---
+++ functions/location.js
@@ -0,0 +1,43 @@
+const Table = require('cli-table');
+const chalk = require('chalk');
+
+const table = new Table({
+  chars: {
+    top: '═',
+    'top-mid': '╤',
+    'top-left': '╔',
+    'top-right': '╗',
+    bottom: '═',
+    'bottom-mid': '╧',
+    'bottom-left': '╚',
+    'bottom-right': '╝',
+    left: '║',
+    'left-mid': '╟',
+    mid: '─',
+    'mid-mid': '┼',
+    right: '║',
+    'right-mid': '╢',
+    middle: '│'
+  }
+});
+table.push(
+  ['', '', '', '', '', ''],
+  ['', 1, 2, 3, 4, 5],
+  ['', 6, 7, 8, 9, 10],
+  ['', 11, 12, 13, 14, 15],
+  ['', 16, 17, 18, 19, 20],
+  ['', 21, 22, 23, 24, 25]
+);
+
+console.log(table.toString());
+
+const location = (x, y) => {
+  table[x][y] = chalk.bgBlue(table[x][y]);
+  console.clear();
+  console.log(table.toString());
+};
+
+module.exports = {
+  location
+}
+;
--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const moving = require('./functions/movement');
 const contentArr = require('./functions/content');
 const context = require('./functions/context');
 const opLife = require('./functions/life');
-
+const location = require('./functions/location');
 const main = () => {
   console.log('--------------- RETTENET A FÖLD ALATT-------------------');
   console.log(' ');
-
   console.log('Nem is sjtetted hogy Nagyapád birtoka ilyen veszélyeket is rejt. \n Sajognak a csontjaid az esés miatt de szerencsére minden végtagod mozog. \n Ahogy az elméd kezd tisztulni rádöbbensz hogy olcsón megúsztad a zuhanást. \nKorom sötét van körülötted. Gyorsan a zsebedbe nyúlsz és előkapod a telefonodat. \nSzerencsére nem tört össze és tökéletesen működik. \nSajnos ilyen mélyen a föld alatt nincs térerő. De legalább világítani tudsz vele. Viszont a 22%os töltötség nem túl bizalomgerjesztő. \nAhogy körülnézel a telefonod fényében látod egy  régen az épület alá vájt kazamatarendszerbe kerültél ami egymást keresztező folyosók végeláthatatlan labirintusa. \nSajnos lehetetlen ott kimásznod ahonan lezuhantál. Az omlás túl magasan van. \nA légmozgás azonban arra utal hogy valahol kell, hogy legyen egy másik kijárata ennek a helynek. \nLassan azt is észreveszed hogy egy különös borzongató érzés kezd úrrálenni rajtad. Megrázod magad amitől megszűnik a hideg érzet de nem tudsz szabadulnia gondolattól \nhogy valami különös titokkal találtad szembe megad. \nNincs más hátra elindulsz a ősi labirintusban. \nMeg kell találnod a kiutat mielőtt lemerült a telefonod vagy mielőtt … de ebbe inkább nem is gondolsz bele');
 
   const mapArr = map.generateMap(5, 5);
@@ -20,12 +19,9 @@ const main = () => {
   console.log(' ');
 
   console.log('LIFE:', mLife);
-
   map.fillMap(mapArr, shuffledArr);
 
   const characterLocation = map.placeCharacter(mapArr);
-
-  // console.log(`Az X:${characterLocation.x}, Y:${characterLocation.y} koordinátán állsz!`);
 
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -40,21 +36,25 @@ const main = () => {
       moving.mov(mapArr, key, characterLocation);
       console.log(context.returnCon().content.description);
       console.log('LIFE:', mLife);
+      location.location(context.returnCon().x, context.returnCon().y);
     } else if (key === 'a') {
       console.clear();
       moving.mov(mapArr, key, characterLocation);
       console.log(context.returnCon().content.description);
       console.log('LIFE:', mLife);
+      location.location(context.returnCon().x, context.returnCon().y);
     } else if (key === 's') {
       console.clear();
       moving.mov(mapArr, key, characterLocation);
       console.log(context.returnCon().content.description);
       console.log('LIFE:', mLife);
+      location.location(context.returnCon().x, context.returnCon().y);
     } else if (key === 'd') {
       console.clear();
       moving.mov(mapArr, key, characterLocation);
       console.log(context.returnCon().content.description);
       console.log('LIFE:', mLife);
+      location.location(context.returnCon().x, context.returnCon().y);
     }
     console.log(' ');
 
